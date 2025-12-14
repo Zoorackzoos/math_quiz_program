0c4bb63cf8fae35795e9f100dda8b81a5defac20
Duncan Holmes <dholmes4@huskers.unl.edu>
2025-12-14 03:11:30 -0600

    #4 mid feature

--- src/GUI/GUI_sub_files/MainMenuModule_folder/MainMenuModule.py
+++ src/GUI/GUI_sub_files/MainMenuModule_folder/MainMenuModule.py
@@ -1,7 +1,8 @@
+#MainMenuModule.py
+from kivy.app import App
 from kivy.core.window import Window
 from kivy.modules import inspector
 from kivy.uix.screenmanager import Screen
-
 """
 i have this in camelCase because the kivy naming conventions are that way.
 """
@@ -12,6 +13,11 @@ class MainMenuModule(Screen):
         # return Builder.load_file("navigation.kv")
 
 class MainMenuScreen(Screen):
+
+    def printModuleSelection(self):
+        app = App.get_running_app()
+        worksheet_id = app.current_worksheet
+        print("MainMenuScreen -> printModuleSelection -> " + str(worksheet_id))
 
     def test_function(self):
         print("test_function")
---
+++ src/logic/question_variables/testing_module_questions.py
@@ -0,0 +1,21 @@
+testing_module_questions = \
+        [
+            {
+                "title": "Q1",
+                "LaTex": r"\frac{d}{dx}(x^2)",
+                "answer": "2x",
+                "tolerance": 0.01
+            },
+            {
+                "title": "Q2",
+                "LaTex": r"This is question 2! please answer, '2' in the answer box.",
+                "answer": "2",
+                "tolerance": 0.01
+            },
+            {
+                "title": "Q3",
+                "LaTex": r"This is question 3! please answer, '3' in the answer box.",
+                "answer": "3",
+                "tolerance": 0.01
+            }
+        ]
